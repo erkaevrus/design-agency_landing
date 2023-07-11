7fc586b134b000021cb5ce97a0ec57f00bf06068
erkaevrus <erk_08@mail.ru>
2023-07-11 16:34:45 +0400

    feat: add experience section, done section, reviews section, add switcher component

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ import '@/index.html'
 
 // ---SVG---
 import '@/assets/sprites/dots.svg'
+import '@/assets/sprites/line-1.svg'
+import '@/assets/sprites/line-2.svg'
+import '@/assets/sprites/quote.svg'
+import '@/assets/sprites/arrow-left.svg'
+import '@/assets/sprites/arrow-right.svg'
